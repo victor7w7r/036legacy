a5b46f12f4f21610bdd48803ec9f1f70fec16c10
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-07-23 20:54:30 -0500

    RemoveBaseFile

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import prefetch from '@astrojs/prefetch';
 
 export default defineConfig({
   site: 'https://victor7w7r.github.io',
-  base: '/036legacy',
   integrations: [compress(), prefetch()]
 });
