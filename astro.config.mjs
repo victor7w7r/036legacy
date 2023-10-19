9270607189eade9356d10d531f19dca2b7389f86
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-10-19 17:07:45 -0500

    PutActionsForPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import compress from 'astro-compress';
 import prefetch from '@astrojs/prefetch';
 
 export default defineConfig({
+  site: 'https://victor7w7r.github.io',
+  base: '/036legacy',
   integrations: [compress(), prefetch()]
 });
