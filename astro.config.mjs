101c8b9a26d774dddec00c5a9d906653c63a11bc
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-12-05 12:58:29 -0500

    RemoveCarbonium

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import compress from 'astro-compress';
 
 export default defineConfig({
   prefetch: true,
-  site: 'https://victor7w7r.github.io',
-  base: '/036legacy',
   integrations: [compress()]
 });
