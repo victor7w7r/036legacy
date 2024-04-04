3cdf18a5a4c4ee6042e56aac956a5bda68fdafbb
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-04-03 19:17:19 -0500

    UpdateEslintRules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
-
-import compress from 'astro-compress';
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
 
 export default defineConfig({
-  prefetch: true,
-  integrations: [compress()]
-});
+  integrations: [compress()],
+  prefetch: true
+})
